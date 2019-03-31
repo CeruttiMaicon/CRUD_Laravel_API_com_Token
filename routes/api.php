23cb3efe44059a7154bf3a800b72f6cbd1069f35
Maicon Cerutti <dev.cerutti.maicon@gmail.com>
2019-03-31 11:36:49 -0300

    Ajustado rotas para deixar a API no padrão RESTFULL

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
+// Route::group(['middleware' => 'auth:api'], function(){
+//     Route::post('details', 'API\UserController@details');
+//     Route::resource('empresa', 'API\EmpresaController');
+ 
+// });
+
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('details', 'API\UserController@details');
-    Route::post('empresas', 'API\EmpresaController@index');
-    Route::post('empresa/create', 'API\EmpresaController@store');
-    Route::post('empresa/edit', 'API\EmpresaController@update');
-    Route::post('empresa/destroy', 'API\EmpresaController@destroy');
+
+    Route::get('empresa', 'API\EmpresaController@index');
+    Route::get('empresa/{id}', 'API\EmpresaController@show');
+    Route::post('empresa', 'API\EmpresaController@store');
+    Route::match(['put', 'patch', 'post'], 'empresa/{id}', 'API\EmpresaController@update');
+
+    Route::delete('empresa/{id}', 'API\EmpresaController@destroy');
+ 
 });
